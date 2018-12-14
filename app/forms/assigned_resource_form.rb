1fc28fc391743de5e2fc5761ff5278329d9b8a2a
Ivan Zablotsky <zablockij_iv_12@mf.grsu.by>
2018-12-14 18:30:56 +0300

    remove calendar days from the project

--- app/forms/assigned_resource_form.rb
+++ app/forms/assigned_resource_form.rb
@@ -11,19 +11,6 @@ class AssignedResourceForm < BaseForm
   def save!
     resource = AssignedResource.new(assigned_resource_params)
     resource.save!
-
-    resource.project_days.destroy_all
-    resource.working_days.each do |day|
-      calendar_day = find_calendar_day(day.to_date)
-      project_day = ProjectDayForm.new(
-        calendar_day_id: calendar_day.id,
-        assigned_resource_id: resource.id,
-        resource_id: resource_id,
-        project_id: project_id
-      )
-
-      raise ActiveRecord::Rollback unless project_day.save
-    end
   end
 
   def assigned_resource_params
@@ -36,9 +23,5 @@ class AssignedResourceForm < BaseForm
       involvement: involvement,
       forecast_type_id: forecast_type_id
     }
-  end
-
-  def find_calendar_day(date)
-    Calendar::Day.find_or_create_by!(date: date)
   end
 end
